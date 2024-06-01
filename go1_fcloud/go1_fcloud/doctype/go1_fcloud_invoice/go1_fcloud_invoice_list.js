19df6ae638d7b43665011faf0ec8f009e10693d3
Tridots Tech <info@tridotstech.com>
2024-06-01 09:46:34 +0000

    Changes

--- go1_fcloud/go1_fcloud/doctype/go1_fcloud_invoice/go1_fcloud_invoice_list.js
+++ go1_fcloud/go1_fcloud/doctype/go1_fcloud_invoice/go1_fcloud_invoice_list.js
@@ -5,7 +5,7 @@ frappe.listview_settings['Go1 FCloud Invoice'] = {
         listview.page.add_inner_button('Sync Invoice', function (frm) {
             frappe.call({
                 method: "go1_fcloud.go1_fcloud.doctype.go1_fcloud_invoice.go1_fcloud_invoice.sync_invoices_queue",
-                async: true,
+                async: false,
                 callback: function (r) {
                     frappe.msgprint("FCloud Invoice Synced Scheduled Successfully")
                     setTimeout(function () {
